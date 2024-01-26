b6bc5e8cb93a16b2763e3d2150d73f27146a39f9
james@dyerdwelling.family <james@dyerdwelling.family>
2024-01-26 15:53:33 +0000

    incorporated garuda-update

--- upall.sh
+++ upall.sh
@@ -5,7 +5,18 @@ echo "----------------------------------------"
 echo "system update"
 echo "----------------------------------------"
 echo
+<<<<<<< Updated upstream
 sudo pacman -Syu --noconfirm
+=======
+if command -v garuda-update &> /dev/null ; then
+   echo "----------------------------------------"
+   echo "garuda-update"
+   echo "----------------------------------------"
+   sudo garuda-update
+else
+   sudo pacman -Syu --noconfirm
+fi
+>>>>>>> Stashed changes
 echo
 echo "----------------------------------------"
 echo "flatpak update"
