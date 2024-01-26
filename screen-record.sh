e5e5142b5c299f8f442a0487d05219871ba84528
james@dyerdwelling.family <james@dyerdwelling.family>
2024-01-26 15:56:49 +0000

    added better screen recording toggle for sway

--- screen-record.sh
+++ screen-record.sh
@@ -9,9 +9,22 @@
 # OpenGLIsUnsafe=false
 DATE=$(date +%Y%m%d%H%M%S)
 DST=~/Videos
-# for sway
+
 if [[ $XDG_SESSION_TYPE == "wayland" ]]; then
-   wf-recorder -f "$DST"/$DATE--screen-recording.mp4 -F fps=10
+   # Note : to get the audio device run: pactl list sources
+   # | grep -i "Name" and choose the first line staring with
+   # Name which will be the very long name!  oh and if you
+   # are not sure which audio device is coming out of then
+   # use pavucontrol
+   pid=`pgrep wf-recorder`
+   status=$?
+
+   if [ $status != 0 ]
+   then
+      wf-recorder --audio="alsa_output.usb-Synaptics_Hi-Res_Audio_000000000000000000000000-00.analog-stereo.monitor" -f "$DST"/$DATE--screen-recording.mp4 -F fps=10
+   else
+      pkill --signal SIGINT wf-recorder
+   fi;
 else
    # for X11 only
    ffmpeg -video_size 1920x1080 -framerate 10 -f x11grab -i :0.0+0,0 -c:v libx264rgb -crf 23 -preset ultrafast "$DST"/$DATE--screen-recording.mp4
