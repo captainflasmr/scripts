b9f070c14ad80efc8cae2de53147cbefe7eeeabf
james@dyerdwelling.family <james@dyerdwelling.family>
2024-03-23 17:19:41 +0000

    fixed and cleaned sticky file generator

--- keymap-sticky-generate.sh
+++ keymap-sticky-generate.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# if [[ $XDG_SESSION_TYPE == "x11" ]]; then
-#    DST=$HOME/.config
-# elif [[ $XDG_SESSION_TYPE == "wayland" ]]; then
-#    DST=$HOME/.config/sway
-# fi
-
 DST=$HOME/.config
 
 # Set keyboard layout to British
@@ -14,7 +8,7 @@ setxkbmap gb
 # Apply sticky modifier changes and save to a specific file
 xkbcomp $DISPLAY -xkb - | \
 sed 's|SetMods|LatchMods|g' > \
-$DST/$keymap_with_sticky_modifiers.xkb
+$DST/keymap_with_sticky_modifiers.xkb
 
 # Reset keyboard layout to British
 setxkbmap gb
