350138ed685e37577849530d967ec64cb83e422f
james@dyerdwelling.family <james@dyerdwelling.family>
2024-03-23 17:19:18 +0000

    improved screen recorder formatting

--- screen-record.sh
+++ screen-record.sh
@@ -21,11 +21,25 @@ if [[ $XDG_SESSION_TYPE == "wayland" ]]; then
 
    if [ $status != 0 ]
    then
-      wf-recorder --audio="alsa_output.usb-Synaptics_Hi-Res_Audio_000000000000000000000000-00.analog-stereo.monitor" -f "$DST"/$DATE--screen-recording.mp4 -F fps=10
+      # wf-recorder \
+      #    --audio="alsa_output.usb-Synaptics_Hi-Res_Audio_000000000000000000000000-00.analog-stereo.monitor" \
+      #    -f "$DST"/$DATE--screen-recording.mp4 -F fps=5
+      wf-recorder \
+         --audio="alsa_output.usb-Synaptics_Hi-Res_Audio_000000000000000000000000-00.analog-stereo.monitor" \
+         -f "$DST"/$DATE--screen-recording.mkv -F fps=5
    else
       pkill --signal SIGINT wf-recorder
    fi;
 else
-   # for X11 only
-   ffmpeg -video_size 1920x1080 -framerate 10 -f x11grab -i :0.0+0,0 -c:v libx264rgb -crf 23 -preset ultrafast "$DST"/$DATE--screen-recording.mp4
+   pid=`pgrep ffmpeg`
+   status=$?
+
+   if [ $status != 0 ]
+   then
+      ffmpeg -video_size 1920x1080 -framerate 10 \
+             -f x11grab -i :0.0+0,0 -c:v libx264rgb -crf 23 \
+             -preset ultrafast "$DST"/$DATE--screen-recording.mp4
+   else
+      pkill --signal SIGINT ffmpeg
+   fi;
 fi
