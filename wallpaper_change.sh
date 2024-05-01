e50ff04601850c3c0862e3bd1f5ed11dc6200ede
james@dyerdwelling.family <james@dyerdwelling.family>
2024-05-01 15:09:03 +0100

    Improve wallpaper selection efficiency in wallpaper_change.sh
    - Centralize wallpaper path selection to occur once at the beginning.
    - Save the chosen wallpaper path to ~/.last_wallpaper_path and copy the file to ~/.last_wallpaper.jpg for easier retrieval.
    - Simplify conditional branches by using the pre-selected wallpaper path instead of finding a new one each time.

--- wallpaper_change.sh
+++ wallpaper_change.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+wallpaper_path="$(find ~/wallpaper -type f | shuf -n 1)"
+echo $wallpaper_path > ~/.last_wallpaper_path
+cp -f $wallpaper_path ~/.last_wallpaper.jpg
+
 if [[ $XDG_SESSION_TYPE == "x11" ]]; then
-   # feh --bg-scale "$(find ~/wallpaper -type f | shuf -n 1)"
-   wallpaper_path="$(find ~/wallpaper -type f | shuf -n 1)"
-   echo $wallpaper_path > ~/.last_wallpaper_path
    feh --bg-scale "$wallpaper_path"
 fi
 
 if [[ $XDG_SESSION_TYPE == "wayland" ]]; then
-   swww img $(find ~/wallpaper -type f | shuf -n 1) --transition-step 20 --transition-fps=20
+   # swww img "/home/jdyer/wallpaper/wallpaper-sddm.jpg" --transition-step 20 --transition-fps=20
+   swww img "$wallpaper_path" --transition-step 20 --transition-fps=20
 fi
