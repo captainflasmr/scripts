d0c7bb430b4ee427e4dcfb577a01de29f15e1c8d
james@dyerdwelling.family <james@dyerdwelling.family>
2024-05-16 09:39:12 +0100

    Update symbol representation for keymap status in keymap-monitor.sh
    Changes include:
    - Replaced the placeholder text 'l' and 'L' with more intuitive and visually distinct symbols for active and inactive keymap states.
      - For an active keymap state, now using "🔗" to symbolize connection or linkage.
      - For an inactive keymap state, changed to "⛓️" to indicate a lock or secured status, enhancing the visual feedback's clarity.

--- keymap-monitor.sh
+++ keymap-monitor.sh
@@ -7,9 +7,11 @@ CURRENT_KEYMAP=$(cat "$CURRENT_KEYMAP_PATH" | grep "sticky")
 if [[ $CURRENT_KEYMAP ]]; then
    # echo "{\"text\": \"🗝️\", \"class\": \"active\"}"
    # echo "{\"text\": \"\", \"class\": \"active\"}"
-   echo "{\"text\": \"l\", \"class\": \"active\"}"
+   # echo "{\"text\": \"l\", \"class\": \"active\"}"
+   echo "{\"text\": \"🔗\", \"class\": \"active\"}"
 else
    # echo "{\"text\": \"🔒\", \"class\": \"inactive\"}"
    # echo "{\"text\": \"\", \"class\": \"inactive\"}"
-   echo "{\"text\": \"L\", \"class\": \"inactive\"}"
+   # echo "{\"text\": \"L\", \"class\": \"inactive\"}"
+   echo "{\"text\": \"⛓️\", \"class\": \"inactive\"}"
 fi
