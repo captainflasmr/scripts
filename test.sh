d8561ff6b58df3b4299125ec39f083327c26a8ca
james@dyerdwelling.family <james@dyerdwelling.family>
2024-06-13 07:30:07 +0100

    updating test script

--- test.sh
+++ test.sh
@@ -1,30 +1,12 @@
 #!/bin/bash
-echo "poop: 4"
-# setKeyboardLight () {
-    dbus-send --system --type=method_call --dest="org.freedesktop.UPower" "/org/freedesktop/UPower/KbdBacklight" "org.freedesktop.UPower.KbdBacklight.SetBrightness" int32:0
-echo "poop: 3"
-# }
 
-# setKeyboardLight 0
+# The cron job you want to add
+CRON_JOB="@reboot /home/jdyer/bin/startup_root.sh"
 
-# if pgrep "^wf-recorder$" >/dev/null; then echo '{"class": "recording"}'; fi
-
-# if pgrep "^wf-recorder$" >/dev/null; then
-#    echo '{\"class\": \"recording\"}'
-# else
-#    echo '{\"class\": \"ready\"}'
-# fi
-
-# if pgrep "^wf-recorder$" >/dev/null; then
-#    echo '{\"class\": \"recording\"}'
-# fi
-
-# if pgrep "^wf-recorder$" >/dev/null; then
-#    echo "poop: 1"
-# else
-#    echo "poop: 2"
-# fi
-
-   # if [[ ! $XDG_CURRENT_DESKTOP == "KDE" ]]; then
-#    echo "poop: 1"
-# fi
+# Check if the cron job already exists
+if ! sudo crontab -l | grep -Fxq "$CRON_JOB"; then
+    (sudo crontab -l; echo "$CRON_JOB") | sudo crontab -
+    echo "Cron job added successfully."
+else
+    echo "Cron job already exists. No action taken."
+fi
