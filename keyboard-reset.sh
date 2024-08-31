3fd6158312e8ce3e1b0ce11ba8899aa949c8749c
james@dyerdwelling.family <james@dyerdwelling.family>
2024-08-31 16:32:51 +0100

    refactor: comment out notify-send messages in keyboard-reset.sh
    - Disabled `notify-send` for keyboard connection and disconnection notifications.

--- keyboard-reset.sh
+++ keyboard-reset.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [[ -L "/dev/input/by-id/usb-SEMICO_USB_Gaming_Keyboard-event-kbd" ]]; then
-   notify-send -t 3000 "KEYBOARD CONNECTED!"
+   # notify-send -t 3000 "KEYBOARD CONNECTED!"
    swaymsg input 1:1:AT_Translated_Set_2_keyboard events disabled
 else
-   notify-send -t 3000 "KEYBOARD DISCONNECTED!"
+   # notify-send -t 3000 "KEYBOARD DISCONNECTED!"
    swaymsg input 1:1:AT_Translated_Set_2_keyboard events enabled
 fi
