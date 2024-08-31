9bffb8bd54e5fe381207c9e49e23176038fd319d
james@dyerdwelling.family <james@dyerdwelling.family>
2024-08-31 16:27:23 +0100

    fix: correct keymap file in keymap-load.sh
    - Changed keymap file from `keymap_with_locked_modifiers.xkb` to `keymap_with_sticky_modifiers.xkb`.

--- keymap-load.sh
+++ keymap-load.sh
@@ -8,7 +8,7 @@ echo "DISPLAY=$DISPLAY" >> /tmp/xkb_script.log
 if [ -z "$DISPLAY" ]; then
     export DISPLAY=:0
 fi
-xkbcomp $HOME/.config/keymap_with_locked_modifiers.xkb $DISPLAY
+xkbcomp $HOME/.config/keymap_with_sticky_modifiers.xkb $DISPLAY
 # setxkbmap -layout $HOME/.config/keymap_with_locked_modifiers.xkb -option ctrl:nocaps
 
 echo "Setxkbmap command run" >> /tmp/xkb_script.log
