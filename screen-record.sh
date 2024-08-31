af01a1f87b3d60aaf725d6f05c2f2aba93196ad4
james@dyerdwelling.family <james@dyerdwelling.family>
2024-08-31 16:32:12 +0100

    refactor: update screen recording script for Wayland and X11
    - Switched default screen recorder on Wayland to `wf-recorder` from `wl-screenrec`.
    - Updated `wf-recorder` to include audio and set fps to 5.
    - Changed ffmpeg framerate from 10 to 30 for X11.
    - Improved readability by removing commented code.

--- screen-record.sh
+++ screen-record.sh
@@ -8,32 +8,28 @@ if [[ $XDG_SESSION_TYPE == "wayland" ]]; then
    # Name which will be the very long name!  oh and if you
    # are not sure which audio device is coming out of then
    # use pavucontrol
-   # pid=`pgrep wf-recorder`
-   pid=`pgrep wl-screenrec`
+   pid=`pgrep wf-recorder`
+   # pid=`pgrep wl-screenrec`
    status=$?
 
    if [[ $status != 0 ]]; then
-
-      # wf-recorder \
-      #    --audio="alsa_output.usb-Synaptics_Hi-Res_Audio_000000000000000000000000-00.analog-stereo.monitor" \
-      #    -f "$DST"/$DATE--screen-recording.mkv -F fps=5
-
-      wl-screenrec \
-         -f "$DST"/$DATE--screen-recording.mkv
+      wf-recorder \
+         --audio="alsa_output.usb-Synaptics_Hi-Res_Audio_000000000000000000000000-00.analog-stereo.monitor" \
+         -f "$DST"/$DATE--screen-recording.mkv -F fps=5
+      # wl-screenrec \
+         # -f "$DST"/$DATE--screen-recording.mkv
    else
-      # pkill --signal SIGINT wf-recorder
-      pkill --signal SIGINT wl-screenrec
+      pkill --signal SIGINT wf-recorder
+      # pkill --signal SIGINT wl-screenrec
    fi;
 else
    pid=`pgrep ffmpeg`
    status=$?
 
    if [[ $status != 0 ]]; then
-
-      ffmpeg -video_size 1920x1080 -framerate 10 \
+      ffmpeg -video_size 1920x1080 -framerate 30 \
              -f x11grab -i :0.0+0,0 -c:v libx264rgb -crf 23 \
              -preset ultrafast "$DST"/$DATE--screen-recording.mp4
-
    else
       pkill --signal SIGINT ffmpeg
    fi;
