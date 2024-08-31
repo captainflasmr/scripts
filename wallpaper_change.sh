d740fe5a8c6d6591558c53df0db28b976f287acb
james@dyerdwelling.family <james@dyerdwelling.family>
2024-08-31 16:31:28 +0100

    feat: enhance wallpaper change script with color scheme generation
    - Changed wallpaper directory search to include all files under `~/wallpaper`.
    - Added pywal commands to generate a color scheme from the new wallpaper.
    - Updated Sway window decoration colors based on the generated color scheme.

--- wallpaper_change.sh
+++ wallpaper_change.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wallpaper_path="$(find ~/wallpaper/wallpaper-new -type f | shuf -n 1)"
+wallpaper_path="$(find ~/wallpaper -type f | shuf -n 1)"
 echo $wallpaper_path > ~/.last_wallpaper_path
 cp -f $wallpaper_path ~/.last_wallpaper.jpg
 
@@ -12,3 +12,18 @@ if [[ $XDG_SESSION_TYPE == "wayland" ]]; then
    # swww img "/home/jdyer/wallpaper/wallpaper-sddm.jpg" --transition-step 20 --transition-fps=20
    swww img "$wallpaper_path" --transition-step 20 --transition-fps=20
 fi
+
+# lets run pywal for colour scheme generation
+wal -c
+wal -i ~/.last_wallpaper.jpg -q -n
+
+# Load colors from pywal
+colors=$(cat ~/.cache/wal/colors.json)
+
+# Extracting colors
+fg=$(echo $colors | jq -r '.colors.color7')
+bg=$(echo $colors | jq -r '.colors.color8')
+
+# Update sway window decorations
+swaymsg client.focused "$fg" "$fg" "$fg" "$fg" "$fg"
+swaymsg client.focused_inactive "$bg" "$bg" "$bg" "$bg" "$bg"
