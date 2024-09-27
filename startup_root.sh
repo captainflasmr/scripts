4de0086aaa63a87912746541fd52b143256eb55e
james@dyerdwelling.family <james@dyerdwelling.family>
2024-09-27 17:45:15 +0100

    Enhance Waybar scripts for dynamic configuration management
    - Updated `launch_waybar` to dynamically set Waybar configuration based on the configuration file:
      - Added logic to initialize the configuration file if it doesn't exist.
      - Waybar now starts with specific configuration and style defined in the configuration file.
    
    - Modified `startup_root.sh`:
      - Enabled `xremap` to load Emacs key mappings at startup.
      - Adjusted `cpupower` command for performance tuning (commented out).
    
    - Improved `switch_waybar.sh`:
      - Replaced symlink switching logic with dynamic configuration file handling.
      - Automatically toggles between `waybar_garuda` and `waybar_jdyer` configurations.
      - Starts Waybar with the new configuration after switching.

--- startup_root.sh
+++ startup_root.sh
@@ -4,7 +4,9 @@
 
 # ydotoold --socket-perm 0777 --socket-path=/run/user/1000/.ydotool_socket &
 
-# sudo cpupower frequency-set --governor powersave --max 1000MHz
+# cpupower frequency-set --governor powersave --max 1000MHz
+
+xremap /home/jdyer/.config/xremap/emacs.yml &
 
 while [[ ! -d /home/jdyer/nas/Home ]]; do
    # mount -t nfs captainflasmr:/volume1/Drive /home/jdyer/nas
--- switch_waybar.sh
+++ switch_waybar.sh
@@ -1,16 +1,41 @@
 #!/bin/bash
 
-cd $HOME/.config
+# cd $HOME/.config
 
-if [ "$(readlink "waybar")" = "waybar_jdyer" ]; then
-    ln -sfn "waybar_garuda" "waybar"
-    echo "Switched to waybar_garuda."
-elif [ "$(readlink "waybar")" = "waybar_garuda" ]; then
-    ln -sfn "waybar_jdyer" "waybar"
-    echo "Switched to waybar_jdyer."
-else
-    echo "The symlink does not point to a known location."
+# if [ "$(readlink "waybar")" = "waybar_jdyer" ]; then
+#     ln -sfn "waybar_garuda" "waybar"
+#     echo "Switched to waybar_garuda."
+# elif [ "$(readlink "waybar")" = "waybar_garuda" ]; then
+#     ln -sfn "waybar_jdyer" "waybar"
+#     echo "Switched to waybar_jdyer."
+# else
+#     echo "The symlink does not point to a known location."
+# fi
+
+# killall waybar
+# waybar &
+
+CONFIG_FILE="/home/jdyer/.config/WAYBAR"
+CONFIG_GARUDA="/home/jdyer/.config/waybar_garuda"
+CONFIG_JDYER="/home/jdyer/.config/waybar_jdyer"
+
+if [[ ! -f $CONFIG_FILE ]]; then
+   echo $CONFIG_GARUDA > $CONFIG_FILE
 fi
 
+CURRENT_CONFIG=$(cat $CONFIG_FILE)
+
+if [[ $CURRENT_CONFIG == $CONFIG_GARUDA ]]; then
+   NEW_CONFIG=$CONFIG_JDYER
+else
+   NEW_CONFIG=$CONFIG_GARUDA
+fi
+
+echo $NEW_CONFIG > $CONFIG_FILE
+
 killall waybar
-waybar &
+
+# Start waybar with the new configuration
+waybar -c $NEW_CONFIG/config -s $NEW_CONFIG/style.css &
+
+echo "Switched to $(basename $NEW_CONFIG)"
