643fa21c125e669a916eee3262200bbd01d1bca4
james@dyerdwelling.family <james@dyerdwelling.family>
2024-10-07 10:09:58 +0100

    Enhance: Dynamic configuration selection for toggle_waybar.sh

--- toggle_waybar.sh
+++ toggle_waybar.sh
@@ -4,9 +4,20 @@
 
 # Determine if Waybar is running
 if pgrep -x waybar > /dev/null; then
-    # If Waybar is running, kill it
-    killall waybar
+   # If Waybar is running, kill it
+   killall waybar
 else
-    # If Waybar is not running, start it
-    waybar &
+   # If Waybar is not running, start it
+   CONFIG_FILE="/home/jdyer/.config/WAYBAR"
+   CONFIG_GARUDA="/home/jdyer/.config/waybar_garuda"
+   CONFIG_JDYER="/home/jdyer/.config/waybar_jdyer"
+
+   if [[ ! -f $CONFIG_FILE ]]; then
+      echo $CONFIG_GARUDA > $CONFIG_FILE
+   fi
+
+   CURRENT_CONFIG=$(cat $CONFIG_FILE)
+
+   # Start waybar with the new configuration
+   waybar -c $CURRENT_CONFIG/config -s $CURRENT_CONFIG/style.css &
 fi
