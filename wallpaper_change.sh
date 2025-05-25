ca0aa19bf85cfcb7663f739ce126ae3c39491b49
james dyer <captainflasmr@gmail.com>
2025-05-25 11:15:40 +0100

    switched to swaybg

--- wallpaper_change.sh
+++ wallpaper_change.sh
@@ -5,12 +5,14 @@ echo $wallpaper_path > ~/.last_wallpaper_path
 cp -f $wallpaper_path ~/.last_wallpaper.jpg
 
 if [[ $XDG_SESSION_TYPE == "x11" ]]; then
-   feh --bg-scale "$wallpaper_path"
+    feh --bg-scale "$wallpaper_path"
 fi
 
 if [[ $XDG_SESSION_TYPE == "wayland" ]]; then
-   # swww img "/home/jdyer/wallpaper/wallpaper-sddm.jpg" --transition-step 20 --transition-fps=20
-   swww img "$wallpaper_path" --transition-step 20 --transition-fps=20
+    # swww img "/home/jdyer/wallpaper/wallpaper-sddm.jpg" --transition-step 20 --transition-fps=20
+    # swww img "$wallpaper_path" --transition-step 20 --transition-fps=20
+    pkill swaybg
+    swaybg -i "$wallpaper_path" &
 fi
 
 # lets run pywal for colour scheme generation
