9014653f4d9d291bfe5519bbe66ae5e5ef2d10a8
james dyer <captainflasmr@gmail.com>
2026-02-21 09:32:31 +0000

    changed nas IP

--- startup_root.sh
+++ startup_root.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 while [[ ! -d /home/jdyer/nas/Home ]]; do
-   mount -v -t nfs 192.168.0.19:/volume1/Drive /home/jdyer/nas
+   # mount -v -t nfs 192.168.0.11:/volume1/Drive /home/jdyer/nas
+   mount -v -t nfs 192.168.0.10:/volume1/Drive /home/jdyer/nas
    sleep 2
 done
