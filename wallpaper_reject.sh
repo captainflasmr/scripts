2bffe6a32ff12c594d7b218a9986b8f54a5b2088
james dyer <captainflasmr@gmail.com>
2026-05-19 07:07:26 +0100

    wallpaper improvements

--- wallpaper_reject.sh
+++ wallpaper_reject.sh
@@ -1,32 +1,39 @@
 #!/bin/bash
 
-wallpaper_path="$(find ~/wallpaper -type f | shuf -n 1)"
+current_path=$(cat ~/.last_wallpaper_path 2>/dev/null)
+previous_path=$(cat ~/.previous_wallpaper_path 2>/dev/null)
 
-# Save current wallpaper as previous before overwriting
-if [ -f ~/.last_wallpaper_path ]; then
-    cp ~/.last_wallpaper_path ~/.previous_wallpaper_path
+if [ -z "$current_path" ]; then
+    notify-send "No wallpaper to reject"
+    exit 1
 fi
 
-echo $wallpaper_path > ~/.last_wallpaper_path
-cp -f $wallpaper_path ~/.last_wallpaper.jpg
-
-if [[ $XDG_SESSION_TYPE == "x11" ]]; then
-    feh --bg-scale "$wallpaper_path"
+if [ -z "$previous_path" ]; then
+    notify-send "No previous wallpaper to restore"
+    exit 1
 fi
+
+if [ ! -f "$previous_path" ]; then
+    notify-send "Previous wallpaper file not found: $previous_path"
+    exit 1
+fi
+
+rm -f "$current_path"
+
+echo "$previous_path" > ~/.last_wallpaper_path
+cp -f "$previous_path" ~/.last_wallpaper.jpg
+
+> ~/.previous_wallpaper_path
 
 if [[ $XDG_SESSION_TYPE == "wayland" ]]; then
     pkill swaybg
-    swaybg -i "$wallpaper_path" &
+    swaybg -i "$previous_path" &
 fi
 
-# lets run pywal for colour scheme generation
 wal -c
-# wal -i ~/.last_wallpaper.jpg -q -n 2>&1 /dev/null
 wal -i ~/.last_wallpaper.jpg -q -n
 
-# Load colors from pywal
 colors=$(cat ~/.cache/wal/colors.json)
-
 fg=$(echo $colors | jq -r '.special.foreground')
 bg=$(echo $colors | jq -r '.special.background')
 c1=$(echo $colors | jq -r '.colors.color1')
@@ -35,8 +42,9 @@ c5=$(echo $colors | jq -r '.colors.color5')
 c6=$(echo $colors | jq -r '.colors.color6')
 c8=$(echo $colors | jq -r '.colors.color8')
 
-# Update sway window decorations
 swaymsg client.focused          "$fg" "$bg" "$fg" "$c5" "$c6"
 swaymsg client.focused_inactive "#666666" "#2a2a2a" "#888888" "#666666" "#666666"
 swaymsg client.unfocused         "#555555" "#222222" "#777777" "#555555" "#555555"
 swaymsg client.urgent            "$c1" "$c1" "$bg" "$fg" "$c1"
+
+notify-send "Wallpaper rejected and deleted" "$(basename "$current_path") removed"
